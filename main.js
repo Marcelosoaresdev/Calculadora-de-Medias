43980c09478c8bf9ca2fc09adb8fda3ad5a6c908
Marcelo Soares <marcelohsoares142@gmail.com>
2024-05-15 13:02:55 -0300

    Adicionado a função de Média Final

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const form = document.getElementById ('form-atividade')
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />'
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />'
+const atividades = []
+const notas = []
 
 let linhas = '' /* Linhas comeca vazia */
 
@@ -9,11 +11,15 @@ form.addEventListener ('submit', function (e){
    
     adicionaLinha()
     atualizaTabela()
+    atualizaMediaFinal()
 }) 
 
 function adicionaLinha() {
     const inputNomeAtividade = document.getElementById ('nome-atividade')
     const inputNotaAtividade = document.getElementById ('nota-atividade')
+
+    atividades.push(inputNomeAtividade.value)
+    notas.push(parseFloat(inputNotaAtividade.value)) /* Transforma o valor que sair de inputNotaAtividade(String) em um tipo numérico e faz o push para dentro do array notas*/
 
     let linha = '<tr>'   // <tr> = Linha da tabela
     linha += `<td>${inputNomeAtividade.value}</td>`     // <td> = Coluna da Tabela
@@ -30,4 +36,16 @@ function adicionaLinha() {
 function atualizaTabela () {
     const corpoTabela = document.querySelector('tbody')
     corpoTabela.innerHTML = linhas  /* E Linhas escreve na tela todo os dados, e volta valendo uma string vazia que faz com que o elemento nao possa ser redefinido, e sim, atribuido*/
+}
+
+function atualizaMediaFinal() {
+    let somaDasNotas = 0
+    
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    const media = somaDasNotas / notas.length
+
+    console.log(somaDasNotas)
 }
